acd2e362e139953f896fb2dfac1f2a43864ba001
Jaideep Singh Kandari <jaideepskandari@gmail.com>
2017-01-10 17:01:32 +0530

    record_shorten D8 working [views display plugin not done: +1 new issue]

--- modules/record_shorten/src/Controller/DefaultController.php
+++ modules/record_shorten/src/Controller/DefaultController.php
@@ -19,6 +19,7 @@ class DefaultController extends ControllerBase {
     $total = db_query("SELECT COUNT(sid) FROM {record_shorten}")->fetchField();
 
     $build['summary']['#markup'] = '<p>' . \Drupal::translation()->formatPlural($total, '1 shortened URL has been recorded.', '@count shortened URLs have been recorded.');
+
     $build['records_table']['#markup'] = record_shorten_records_table();
 
     $form = \Drupal::formBuilder()->getForm('Drupal\record_shorten\Form\RecordshortenClearAll');
